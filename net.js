4ef4dd861862b1e7985a62853ea79eeb2d07fcd1
MaitomanDeluxe <maitomandeluxe@outlook.jp>
2026-03-17 10:03:19 +0900

    Update net.js

--- net.js
+++ net.js
@@ -1,30 +1,57 @@
 let socket
+let myId=null
+let lastMove=Date.now()
 
 function startOnline(){
 
-let name=prompt("Player name")
+let name=prompt("名前入力")
+document.getElementById("menu").style.display="none"
 
-socket = new WebSocket(
-  "wss://fpsgame.maitomandeluxe.workers.dev/ws"
-)
+socket=new WebSocket("wss://fpsgame.maitomandeluxe.workers.dev/ws")
 
 socket.onopen=()=>{
-socket.send(JSON.stringify({
-type:"join",
-name:name
-}))
+document.getElementById("status").innerText="接続成功"
+socket.send(JSON.stringify({type:"join",name:name}))
 }
 
 socket.onmessage=e=>{
 let data=JSON.parse(e.data)
 
-if(data.type=="players"){
-console.log(data.players)
+if(data.type==="init"){
+myId=data.id
 }
 
+if(data.type==="players"){
+updatePlayers(data.players)
 }
+
+if(data.type==="kick"){
+alert("AFKでキックされました")
+location.reload()
+}
+}
+
+// AFK監視
+setInterval(()=>{
+if(Date.now()-lastMove>60000){
+socket.send(JSON.stringify({type:"afk"}))
+}
+},5000)
+}
+
+function sendMove(x,z){
+if(!socket)return
+
+lastMove=Date.now()
+
+socket.send(JSON.stringify({
+type:"move",
+x:x,
+z:z
+}))
 }
 
 function startOffline(){
-alert("offline mode")
+document.getElementById("menu").style.display="none"
+document.getElementById("status").innerText="オフライン"
 }
