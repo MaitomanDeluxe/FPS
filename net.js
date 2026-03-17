204666f5ceacdf3040b40ba8356f181ce3da3e00
MaitomanDeluxe <maitomandeluxe@outlook.jp>
2026-03-17 09:40:32 +0900

    Create net.js

---
+++ net.js
@@ -0,0 +1,30 @@
+let socket
+
+function startOnline(){
+
+let name=prompt("Player name")
+
+socket=new WebSocket(
+"wss://YOUR-WORKER.workers.dev/ws"
+)
+
+socket.onopen=()=>{
+socket.send(JSON.stringify({
+type:"join",
+name:name
+}))
+}
+
+socket.onmessage=e=>{
+let data=JSON.parse(e.data)
+
+if(data.type=="players"){
+console.log(data.players)
+}
+
+}
+}
+
+function startOffline(){
+alert("offline mode")
+}
